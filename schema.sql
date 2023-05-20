3b9d87578c5f2e740e94f5bdc30f6e8998987310
Kamil Khan <kamilkhan@rediffmail.com>
2023-05-20 01:01:23 +0100

    few more test records are added

--- schema.sql
+++ schema.sql
@@ -2,15 +2,21 @@ DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS ts;
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL,
-  company  text,
-  role text
+  username varchar(200) UNIQUE NOT NULL,
+  password varchar(200) NOT NULL,
+  company  varchar(200),
+  role varchar(200)
 );
 
 insert into user values (1,"Kamil","a","AGNI","employee");
 insert into user values (3,"Eial","a","AGNI","employee");
 insert into user values (2,"Kaushik","a","AGNI","employer");
+
+insert into user values (1,"Kamil","a","Rizq","employee");
+insert into user values (3,"Anwar","a","Rizq","employee");
+insert into user values (4,"Peer","a","Rizq","employee");
+insert into user values (2,"Ali","a","Rizq","employer");
+
 
 create table ts(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
